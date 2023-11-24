076fc64a83eb43718d18c4d2fcaaa9fba2323e69
gozie11 <gozieo7@gmail.com>
2023-11-24 04:47:11 -0600

    adding transition styling to nav bar links

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,9 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faUser, faHome } from '@fortawesome/free-solid-svg-icons'
 const Sidebar = () => (
     <div className="nav-bar">
         <Link className = 'logo' to = '/'>
@@ -9,6 +11,18 @@ const Sidebar = () => (
             <img className = "sub-logo" src = {LogoSubtitle} alt = 'Gozie' />
         
         </Link>
+        <nav>
+            <NavLink exact = "true" activeclassname="active" to = "/">
+                <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
+            </NavLink>
+            <NavLink exact = "true" activeclassname="about-link" to = "/about">
+                <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
+            </NavLink>
+            <NavLink exact = "true" activeclassname="contact-link" to = "/contact">
+                <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
+            </NavLink>
+
+        </nav>
     </div>
 )
 
