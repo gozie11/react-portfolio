ea58b950dc93b6d3bd02cbeea660b496e8b8048d
gozie11 <gozieo7@gmail.com>
2023-11-24 05:45:27 -0600

    added class name to navlink to fix hover bug

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -15,10 +15,14 @@ const Sidebar = () => (
             <NavLink exact = "true" activeclassname="active" to = "/">
                 <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
             </NavLink>
-            <NavLink exact = "true" activeclassname="about-link" to = "/about">
+            <NavLink 
+                exact = "true" 
+                activeclassname="active" 
+                className="about-link" 
+                to = "/about">
                 <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
             </NavLink>
-            <NavLink exact = "true" activeclassname="contact-link" to = "/contact">
+            <NavLink exact = "true" activeclassname="active" className="contact-link" to = "/contact">
                 <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
             </NavLink>
 
