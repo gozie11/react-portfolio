86a75d428e2711e865fb328f5748e82bec2c6073
gozie11 <gozieo7@gmail.com>
2023-11-25 07:34:13 -0600

    !! letters appear w indiv animations !!

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,7 +4,7 @@ import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = ['G', 'o', 'z', 'i', 'e']
+    const nameArray = [' ','G', 'o', 'z', 'i', 'e', ' ']
     // Below should be called welcome array
     const welcomeArray = ['W', 'e', 'l', 'c', 'o', 'm', 'e', ' ', 't', 'o', ' ', 'm', 'y', ' ', 'p', 'o', 'r', 't', 'f', 'o', 'l', 'i', 'o']
     
@@ -17,16 +17,17 @@ const Home = () => {
                     <span className={`${letterClass} _12`}>i,</span>
                     <br />
                     <span className={`${letterClass} _13`}>I'</span>
-                    <span className={`${letterClass} _14`}>m </span>
+                    <span className={`${letterClass} _14`}>m  </span>
                     <AnimatedLetters letterClass={letterClass} 
                     strArray={nameArray} 
                     idx={15} />
                     {/* <img src={LogoTitle} alt="developer" /> */}
                     
                     <span className="wave-emoji">👋</span>
+                    <br />
                     <AnimatedLetters letterClass={letterClass}
                     strArray={welcomeArray}
-                    idx={22} />
+                    idx={30} />
                 </h1>
                 <h2>Full stack engineer / Python developer / Creative</h2>
                 <Link to="/contact" className="flat-button">CONTACT ME</Link>
