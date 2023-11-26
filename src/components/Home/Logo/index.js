dded8e880a0c3e3225f4730ba3f4988fb248a503
gozie11 <gozieo7@gmail.com>
2023-11-26 16:04:41 -0600

    Trying to get logo to be drawn

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,16 +1,38 @@
 import './index.scss';
 import LogoS from '../../../assets/images/logo-s.png'
-import { useRef } from 'react';
-
+import { useEffect, useRef } from 'react';
+import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
+import gsap from 'gsap-trial';
 
 const Logo = () => {
     const bgRef = useRef();
     const outlineLogoRef = useRef();
     const solidLogoRef = useRef();
 
+    useEffect(() => {
+        gsap.registerPlugin(DrawSVGPlugin)
+
+        gsap.timeline()
+        .to(bgRef.current, {
+            duration: 1,
+            opacity: 1
+        })
+        .from(outlineLogoRef.current, {
+            drawSVG: 0,
+            duration:20,
+        })
+        gsap.fromTo(solidLogoRef.current, {
+            opacity: 0,
+        }, {
+            opacity: 1,
+            delay: 4,
+            duration: 4,
+        })
+    }, [])
+
     return (
-    <div className="logo-container">
-        <img className="solid-logo" src={LogoS} alt='S'/>
+    <div className="logo-container" ref={bgRef}>
+        <img ref={solidLogoRef} className="solid-logo" src={LogoS} alt='S'/>
         <svg
         width="559pt"
         height="897pt"
