02727b4b71b386df88b60309ee1588e6e4f346c1
gozie11 <gozieo7@gmail.com>
2023-11-25 23:34:41 -0600

    styling for logo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = [' ','G', 'o', 'z', 'i', 'e', ' ']
@@ -43,6 +44,7 @@ const Home = () => {
                 <h2>Full stack engineer / Python developer / Creative</h2>
                 <Link to="/contact" className="flat-button">CONTACT ME</Link>
             </div>
+            <Logo />
         </div>
     );
 }
